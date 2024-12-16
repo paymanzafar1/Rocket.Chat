a8e6fdf5b88e4dd98362212674211b238ecf38cd
paymanzafar1 <91053236+paymanzafar1@users.noreply.github.com>
2024-12-16 09:29:12 +0330

    ADA 2

--- apps/meteor/client/hooks/useHasLicenseModule.ts
+++ apps/meteor/client/hooks/useHasLicenseModule.ts
@@ -3,7 +3,7 @@ import type { LicenseModule } from '@rocket.chat/core-typings';
 import { useLicenseBase } from './useLicense';
 
 export const useHasLicenseModule = (licenseName: LicenseModule): 'loading' | boolean => {
-	return true;
+	if (licenseName=='canned-responses' || licenseName=='videoconference-enterprise' || licenseName=='voip-enterprise' || licenseName=='auditing' || licenseName=='livechat-enterprise' || licenseName=='federation' || licenseName=='device-management' || licenseName=='engagement-dashboard' || licenseName=='custom-roles') return true;
 	return (
 		useLicenseBase({
 			select: (data) => data.license.activeModules.includes(licenseName),
